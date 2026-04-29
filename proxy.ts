4a714e925c5d2b4549a26d67bd399775dc169137
muhammed  arshaq tk <arshaqtk4@gmail.com>
2026-04-29 08:49:49 +0530

    fix: rename middleware function export to proxy

--- proxy.ts
+++ proxy.ts
@@ -2,8 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { verifyToken } from "./lib/auth";
 
-export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("token")?.value;
+export async function proxy(req: NextRequest) {pro  const token = req.cookies.get("token")?.value;
 
   if (req.nextUrl.pathname.startsWith("/admin")) {
     if (!token || !(await verifyToken(token))) {
